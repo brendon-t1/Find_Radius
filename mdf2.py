1a8e1ae753573475f105e60b7a7538a9540092c5
Brendon <soupe.aux.tortues@gmail.com>
2022-08-20 16:13:49 -0700

    fixed height parameter for hole calc

--- mdf2.py
+++ mdf2.py
@@ -8,7 +8,7 @@ def radiusForWeight(weight=5, height=.75):
     pi = 3.14159
 
     # volume of the .5" radius hole to be removed
-    hole_volume = volumeCircle(.5, .75)
+    hole_volume = volumeCircle(.5, height)
 
     # weight of hole and total weight needed to account for hole removal
     hole_weight = hole_volume * mdf
@@ -18,9 +18,8 @@ def radiusForWeight(weight=5, height=.75):
     cubic_inches = total_weight / mdf
     print(cubic_inches)
     
-    #circle_radius = sqrt(cubic_inches / 3.14159 * .75)
+    
     circle_radius = (cubic_inches / (pi * height)) **.5
-    #circle_radius = squared_circle_radius**.5
     circle_radius_rounded = round(circle_radius, 2)
     
     return circle_radius_rounded
