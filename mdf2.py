76ad432511d04be6b79417afa9e698a8b579584d
Brendon <soupe.aux.tortues@gmail.com>
2022-08-21 14:50:25 -0700

    new material parameter

--- mdf2.py
+++ mdf2.py
@@ -2,26 +2,25 @@ def volumeCircle(radius, height):
     volume = 3.14159 * (radius * radius) * height
     return volume
 
-def radiusForWeight(weight=5, height=.75):
+def radiusForWeight(weight=5, height=.75, material=.028):
     # weight of mdf per cubic inch and the value of pi
-    mdf = .028
+    #mdf = .028
     pi = 3.14159
 
     # volume of the .5" radius hole to be removed
     hole_volume = volumeCircle(.5, height)
 
     # weight of hole and total weight needed to account for hole removal
-    hole_weight = hole_volume * mdf
+    hole_weight = hole_volume * material
     total_weight = weight + hole_weight
 
     #calc cubic inches needed for weight
-    cubic_inches = total_weight / mdf
+    cubic_inches = total_weight / material
     print(cubic_inches)
-    
     
     circle_radius = (cubic_inches / (pi * height)) **.5
     circle_radius_rounded = round(circle_radius, 2)
     
     return circle_radius_rounded
 
-print(radiusForWeight())
+print(radiusForWeight(5, .75, .5))
