4d211906eec99903175cd8bd9a36cbb9946ce129
Brendon <soupe.aux.tortues@gmail.com>
2022-08-24 15:35:09 -0700

    change bar diameter and labels

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ def volumeCircle(radius, height):
     volume = 3.14159 * (radius * radius) * height
     return volume
 
-def radiusForWeight(weight=5.0, height=.75, material=.028):
+def radiusForWeight(weight=5.0, height=.75, material=.028, bar=.5):
     # weight of mdf per cubic inch and the value of pi
     #mdf = .028
     pi = 3.14159
 
     # volume of the .5" radius hole to be removed
-    hole_volume = volumeCircle(.5, height)
+    hole_volume = volumeCircle(bar, height)
 
     # weight of hole and total weight needed to account for hole removal
     hole_weight = hole_volume * material
@@ -35,26 +35,41 @@ def run():
     weight = weight_entry.get()
     thickness = thickness_entry.get()
     density = material_density_entry.get()
-    answer = radiusForWeight(float(weight), float(thickness), float(density))
+    barS = bar_diameter_entry.get()
+    bar = float(barS) // 2
+    answer = radiusForWeight(float(weight), float(thickness), float(density), float(bar))
     answer_box.insert(0, answer)
 
 
 #answer box
 answer_box = Entry(root, width=33, borderwidth=2)
-answer_box.grid(row=0, column=0, columnspan=3)
+answer_box.grid(row=0, column=1, columnspan=4)
+answer_label = Label(root, text="Radius of Weight:")
+answer_label.grid(row=0, column = 0)
 
 #entry box for weight default is 5
 weight_entry = Entry(root, width=10, borderwidth=2)
-weight_entry.grid(row=1, column=0)
+weight_entry.grid(row=2, column=0)
+weight_label = Label(root, text="Weight Desired")
+weight_label.grid(row=1, column=0)
 #entry box for thickness default is .75
 thickness_entry = Entry(root, width=10, borderwidth=2)
-thickness_entry.grid(row=1, column=1)
+thickness_entry.grid(row=2, column=1)
+thickness_label = Label(root, text="Material Thickness")
+thickness_label.grid(row=1, column=1)
 #entry box for material default is mdf
 material_density_entry = Entry(root, width=10, borderwidth=2)
-material_density_entry.grid(row=1, column=2)
+material_density_entry.grid(row=2, column=2)
+material_label = Label(root, text="Material Density")
+material_label.grid(row=1, column=2)
+#entry box for bar diameter
+bar_diameter_entry = Entry(root, width=10, borderwidth=2)
+bar_diameter_entry.grid(row=2, column=3)
+bar_label = Label(root, text="Bar Diameter")
+bar_label.grid(row=1, column=3)
 
 #button to push to run the program
 button_run = Button(root, text="Run", command=run)
-button_run.grid(row=2, column=1)
+button_run.grid(row=3, column=1, columnspan=2, pady=0)
 
 root.mainloop()
--- mdf2.py
+++ mdf2.py
@@ -2,13 +2,13 @@ def volumeCircle(radius, height):
     volume = 3.14159 * (radius * radius) * height
     return volume
 
-def radiusForWeight(weight=5, height=.75, material=.028):
+def radiusForWeight(weight=5, height=.75, material=.028, bar=.5):
     # weight of mdf per cubic inch and the value of pi
     #mdf = .028
     pi = 3.14159
 
     # volume of the .5" radius hole to be removed
-    hole_volume = volumeCircle(.5, height)
+    hole_volume = volumeCircle(bar, height)
 
     # weight of hole and total weight needed to account for hole removal
     hole_weight = hole_volume * material
